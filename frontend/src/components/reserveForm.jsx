098fc65047b23358b628a63346b9a4443e881ef0
VMila <vi-milanez@hotmail.com>
2025-02-16 15:45:44 -0300

    dando post do form de reserva

--- frontend/src/components/reserveForm.jsx
+++ frontend/src/components/reserveForm.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const ReserveForm = () => {
+  const navigate = useNavigate();
   const [selectedDate, setSelectedDate] = useState("");
   const [startTime, setStartTime] = useState("");
   const [endTime, setEndTime] = useState("");
@@ -54,26 +56,62 @@ const ReserveForm = () => {
     }
   }, [startTime, endTime]);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    try {
+      const response = await fetch("http://localhost:3000/reserve", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          selectedDate,
+          startTime,
+          endTime,
+          price: parseInt(price.replace("R$", "").replace(",00", ""), 10)
+        }),
+      });
+  
+      const status = response.status;
+  
+      if (status === 201) {
+        alert("Reserva realizada com sucesso!");
+        window.location.href = "/"; // Ou use navigate() se estiver usando React Router
+      } else {
+        alert("Erro na reserva, revise os dados e tente novamente!");
+      }
+    } catch (error) {
+      console.error("Erro ao enviar a reserva:", error);
+      alert("Ocorreu um erro. Tente novamente mais tarde.");
+    }
+  };
+  
+
+
   return (
-    <body class="w-full flex flex-col bg-white">
-    <div className="mx-auto w-full max-w-7xl p-5 rounded-lg bg-slate-300 md:bg-gray-200 lg:bg-slate-300 flex flex-col h-screen 
+    <body className="w-full flex flex-col bg-white">
+      <div className="mx-auto w-full max-w-7xl p-5 rounded-lg bg-slate-300 md:bg-gray-200 lg:bg-slate-300 flex flex-col h-screen 
       md:w-3/4 md:h-3/4 md:rounded-2xl md:p-10 md:mt-20 lg:mt-12 lg:p-8 md:border-2 lg:border-2 border-slate-800 mb-2">
-      <div className="flex flex-col lg:flex-row h-1/2">
-        <div className="w-full lg:w-1/2 mt-2 md:mt-0 flex items-center justify-center">
-          <img src="images/quadra1.jpg" className="w-full h-full rounded-lg object-cover border-2 border-slate-800" alt="Quadra" />
+        <form
+        onSubmit={(e) => handleSubmit(e)}
+        >
+        <div className="flex flex-col lg:flex-row h-1/2">
+          <div className="w-full lg:w-1/2 mt-2 md:mt-0 flex items-center justify-center">
+            <img src="images/quadra1.jpg" className="w-full h-full rounded-lg object-cover border-2 border-slate-800" alt="Quadra" />
+          </div>
+          <div className="w-full lg:w-1/2 flex flex-col justify-center lg:p-5 lg:text-2xl">
+            <div className="text-left text-slate-800">
+              <p className="text-xl lg:text-3xl font-bold lg:mb-4 lg:mt-0">Ginásio Poliesportivo UFSCAR</p>
+            </div>
+            <div className="text-left flex items-center gap-1 text-slate-800">
+              <i className="material-icons mr-2 text-base lg:mt-2">location_on</i> R. do Lago - São Carlos, SP
+            </div>
+            <div className="text-left flex items-center gap-1 text-slate-800">
+              <i className="material-icons mr-2 text-base lg:mt-2">schedule</i> 7:00 - 22:00
+            </div>
+          </div>
         </div>
-        <div className="w-full lg:w-1/2 flex flex-col justify-center lg:p-5 lg:text-2xl">
-          <div className="text-left text-slate-800">
-            <p className="text-xl lg:text-3xl font-bold lg:mb-4 lg:mt-0">Ginásio Poliesportivo UFSCAR</p>
-          </div>
-          <div className="text-left flex items-center gap-1 text-slate-800">
-            <i className="material-icons mr-2 text-base lg:mt-2">location_on</i> R. do Lago - São Carlos, SP
-          </div>
-          <div className="text-left flex items-center gap-1 text-slate-800">
-            <i className="material-icons mr-2 text-base lg:mt-2">schedule</i> 7:00 - 22:00
-          </div>
-        </div>
-      </div>
       <div className="flex flex-col justify-center items-center lg:items-start lg:mt-0 lg:mb-0 
       lg:flex-row">
         <div className="w-full lg:w-1/2 items-center justify-center lg:mb-0">
@@ -113,9 +151,10 @@ const ReserveForm = () => {
           >
             Reservar
           </button>
-        </div>
+        </div> 
       </div>
-    </div>
+      </form>
+    </div>  
     </body>
   );
 };
